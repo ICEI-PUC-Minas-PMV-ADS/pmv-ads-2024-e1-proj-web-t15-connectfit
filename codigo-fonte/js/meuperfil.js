424ca3b65ee391df94a0c6a70db0a6a774f019d7
nasson <nassonp@hotmail.com>
2024-05-17 15:25:46 -0300

    inserção de script em Meu Perfil

--- codigo-fonte/js/meuperfil.js
+++ codigo-fonte/js/meuperfil.js
@@ -1,5 +1,29 @@
 function toggleMenu() {
     var menu = document.getElementById("menu");
-    menu.classList.toggle("active");
+    menu.classList.toggle("active")
 }
 
+document.addEventListener('DOMContentLoaded', function() {
+
+    //Simulando dados do usuário
+
+    var userData = {
+        photo: 'img/foto_menu-meuperfil.png',
+        name: 'Sophia',
+        fullName: 'Sophia Ferreira Nunes',
+        age: '25 anos',
+        description: 'Eu sou apaixonada por esportes e academia! Adoro me manter ativa e praticar diferentes atividades, como musculação, corrida, natação e futebol.',
+        gyms: 'Academia Corpo em Forma',
+        location: 'Aimorés - MG'
+    };
+
+    //Inserindo os dados do usuário na página
+    
+    document.getElementById('user-Photo').src = userData.photo;
+    document.getElementById('user-Name').textContent = userData.name;
+    document.getElementById('user-Full-Name').textContent = userData.fullName;
+    document.getElementById('user-Age').textContent = userData.age;
+    document.getElementById('user-Description').textContent = userData.description;
+    document.getElementById('user-Gyms').textContent = userData.gyms;
+    document.getElementById('user-Location').textContent = userData.location;
+});
