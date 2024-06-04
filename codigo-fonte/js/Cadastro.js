aa1ae92dca5e60b1295bbcfa592656d3352a4676
Melyssa Semprini <melyssasemprin14@gmail.com>
2024-06-04 14:58:58 -0300

    Implementação de restrição de idade

--- codigo-fonte/js/Cadastro.js
+++ codigo-fonte/js/Cadastro.js
@@ -5,13 +5,21 @@ function registerUser(event) {
     const nomeExibicao = document.forms['register']['nomeExibicao'].value;
     const email = document.forms['register']['email'].value;
     const senha = document.forms['register']['senha'].value;
+    const idade = parseInt(document.forms['register']['idade'].value);
+
+    // Verifica se a idade é menor que 18
+    if (idade < 18) {
+        alert("Você deve ter pelo menos 18 anos para se registrar.");
+        return; // Interrompe a execução da função
+    }
 
     // Cria um objeto com os dados do usuário
     const user = {
         nome: nome,
         nomeExibicao: nomeExibicao,
         email: email,
-        senha: senha
+        senha: senha,
+        idade: idade
     };
 
     // Converte o objeto para uma string JSON e armazena no localStorage
